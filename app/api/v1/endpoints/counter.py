8b0d15ee1daada71f5518a629a6e9c9d00144da3
Manan21st <manan9346@gmail.com>
2025-02-23 01:32:27 +0530

    feat: implemented application layer caching

--- app/api/v1/endpoints/counter.py
+++ app/api/v1/endpoints/counter.py
@@ -28,7 +28,7 @@ async def get_visits(
 ):
     """Get visit count for a website"""
     try:
-        count = await counter_service.get_visit_count(page_id)
-        return VisitCount(page_id=page_id, count=count)
+        count, memory = await counter_service.get_visit_count(page_id)
+        return VisitCount(page_id=page_id, count=count, memory=memory)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e)) 
--- app/services/visit_counter.py
+++ app/services/visit_counter.py
@@ -6,6 +6,8 @@ from ..core.redis_manager import RedisManager
 class VisitCounterService:
     # Creating class attributes so all instances share the same dict and lock
     lock = asyncio.Lock()
+    cache : Dict[str, Dict[str, Any]] = {}
+    CACHE_TTL = 5 # Five sec TTL for cache
 
     def __init__(self):
         """Initialize the visit counter service with Redis manager"""
@@ -21,6 +23,9 @@ class VisitCounterService:
         # TODO: Implement visit count increment
         async with self.lock:
             count = await self.redis_manager.increment(page_id,1)
+
+            self.cache[page_id] = {"count": count, "last_updated": datetime.now()}
+
             return count
 
 
@@ -36,5 +41,11 @@ class VisitCounterService:
         """
         # TODO: Implement getting visit count
         async with self.lock:
+            curr_time = datetime.now()
+            # Check if the page_id is in cache and the cache is still valid
+            if page_id in self.cache and (curr_time - self.cache[page_id]["last_updated"]).seconds < self.CACHE_TTL:
+                return self.cache[page_id]["count"], "cache"
             count = await self.redis_manager.get(page_id)
-            return count
+
+            self.cache[page_id] = {"count": count, "last_updated": curr_time} if count else {"count": 0, "last_updated": curr_time}
+            return count, "redis"
